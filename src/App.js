19a928949c95cd45b144cb9b2307728053c38766
Lionel Mbayu <Lionel.Mbayu@gmail.com>
2022-09-20 12:31:10 -0500

    Final commit Updated styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import Character from './components/Character'
+import styled from 'styled-components'
+
+
+
+const ContainerDiv = styled.div`
+  display: flex;
+  align-items: center;
+  flex-flow: row wrap;  
+`
+
 const App = () => {
 
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  
 
-  const [ characters, setCharacters] = useState()
+  const [ characters, setCharacters] = useState([])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  
+
+
   useEffect(() =>{
     axios.get("https://swapi.dev/api/people/")
     .then((res)=>{
@@ -22,12 +34,18 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      {characters.map((character, index ) => (
-        <Character
-          key={index}
-          name={character.name}
-        />
-      ))}
+      <ContainerDiv>
+      {
+        characters.map((item, index)=>{
+          return (
+            <Character
+              key={index}
+              name={item.name} 
+            />
+          )
+        })
+      }
+      </ContainerDiv>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,36 @@
 // Write your Character component here
+import React, { useState } from 'react';
+import styled from 'styled-components'
+
+const CharacterDiv = styled.div`
+
+height: 12rem;
+display: inline-block;
+border-radius: 3px;
+border: 1px solid red;
+padding: 0.5rem 0;
+margin: 0.5rem 1rem;
+width: 11rem;
+background: transparent;
+color: white;
+border: 2px solid white;
+
+`
+
+
+
+const Character = (props) => {
+
+
+    const { name } = props
+
+    return(
+        <CharacterDiv>
+            <h1>
+                {name}
+            </h1>
+        </CharacterDiv>
+    );
+};
+
+export default Character;
